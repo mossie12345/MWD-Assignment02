dcdf7a631e92a171e883eed7ab26ecf4b09fb50c
mossie12345 <mossieodonnell@gmail.com>
2014-05-12 22:38:43 +0100

    test

---
+++ node/lib/config.mine.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  secret: 'eC1ReJQBUFz4YHXALTYlXZ7anhoMDI2DazfYWguu2Z4BhHrEwa',
+  twitter: {
+    key: 'FUh5xdudRkN1aPPDErxG1LXys',
+    secret: 'eC1ReJQBUFz4YHXALTYlXZ7anhoMDI2DazfYWguu2Z4BhHrEwa'
+  },
+  facebook: {
+    key: 'KEY',
+    secret: 'SECR'
+  }
+}
